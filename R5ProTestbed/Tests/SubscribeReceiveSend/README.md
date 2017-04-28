2160417b634aba6f94dc57261a28bb28289b4cfc
Todd Anderson <bustardcelly@gmail.com>
2017-04-28 11:00:21 -0400

    add of pub/sub send

---
+++ R5ProTestbed/Tests/SubscribeReceiveSend/README.md
@@ -0,0 +1,6 @@
+#Stream Send
+
+###Example Code
+- ***[PublishSendTest.swift](PublishSendTest.swift)***
+- ***[SubscribeReceiveSendTest.swift](SubscribeReceiveSendTest.swift)***
+
---
+++ R5ProTestbed/Tests/SubscribeReceiveSend/SubscribeReceiveSendTest.swift
@@ -0,0 +1,46 @@
+//
+//  PublishStreamImageTest.swift
+//  R5ProTestbed
+//
+//  Created by Andy Zupko on 12/17/15.
+//  Copyright © 2015 Infrared5. All rights reserved.
+//
+
+import UIKit
+import R5Streaming
+
+@objc(SubscribeReceiveSendTest)
+class SubscribeReceiveSendTest: BaseTest {
+    
+    var uiv : UIImageView? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+
+        
+        setupDefaultR5VideoViewController()
+
+        let config = getConfig()
+        // Set up the connection and stream
+        let connection = R5Connection(config: config)
+        self.subscribeStream = R5Stream(connection: connection)
+        self.subscribeStream!.delegate = self
+        self.subscribeStream?.client = self;
+        
+        currentView?.attach(subscribeStream)
+        
+        
+        self.subscribeStream!.play(Testbed.getParameter(param: "stream1") as! String)
+        
+        
+    }
+    
+    func onStreamSend(msg : String){
+        NSLog("Received the msg: %@", msg)
+        ALToastView.toast(in: self.view, withText:msg)
+    }
+
+
+    
+}
