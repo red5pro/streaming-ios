c866fc99e2fa090ebe9815927beaa5ab114c9542
bustardcelly <bustardcelly@gmail.com>
2019-02-21 14:23:13 +0100

    adding reconnect logic for subscribe telephony interrupt example.

--- R5ProTestbed/Tests/SubscribeTelephonyInterrupt/SubscribeTelephonyInterruptTest.swift
+++ R5ProTestbed/Tests/SubscribeTelephonyInterrupt/SubscribeTelephonyInterruptTest.swift
@@ -12,11 +12,28 @@ import R5Streaming
 @objc(SubscribeTelephonyInterruptTest)
 class SubscribeTelephonyInterruptTest: BaseTest {
     
-    var uiv : UIImageView? = nil
+    var finished = false
+    var publisherIsInBackground = false
+    var publisherIsDisconnected = false
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func onR5StreamStatus(_ stream: R5Stream!, withStatus statusCode: Int32, withMessage msg: String!) {
+        NSLog("Status: %s ", r5_string_for_status(statusCode))
+        let s =  String(format: "Status: %s (%@)",  r5_string_for_status(statusCode), msg)
+        ALToastView.toast(in: self.view, withText:s)
         
-        super.viewDidAppear(animated)
+        if (Int(statusCode) == Int(r5_status_disconnected.rawValue)) {
+            self.cleanup()
+        } else if (Int(statusCode) == Int(r5_status_netstatus.rawValue) && msg == "NetStream.Play.UnpublishNotify") {
+            
+            // publisher has unpublished. possibly from background/interrupt.
+            if (publisherIsInBackground) {
+                publisherIsDisconnected = true
+            }
+            
+        }
+    }
+    
+    func Subscribe(_ name: String) {
         
         setupDefaultR5VideoViewController()
         
@@ -29,17 +46,104 @@ class SubscribeTelephonyInterruptTest: BaseTest {
         
         currentView?.attach(subscribeStream)
         
-        self.subscribeStream!.play(Testbed.getParameter(param: "stream1") as! String)
-        
+        self.subscribeStream!.play(name)
         
     }
     
-    func onStreamSend(msg : String){
-        NSLog("Received the msg: %@", msg)
-        ALToastView.toast(in: self.view, withText:msg)
+    func publisherBackground(msg: String) {
+        NSLog("(publisherBackground) the msg: %@", msg)
+        publisherIsInBackground = true
+        ALToastView.toast(in: self.view, withText:"publishBackground: \(msg)")
     }
     
+    func publisherForeground(msg: String) {
+        NSLog("(publisherForeground) the msg: %@", msg)
+        publisherIsInBackground = false
+        ALToastView.toast(in: self.view, withText:"publisherForeground: \(msg)")
+    }
     
+    func publisherInterrupt(msg: String) {
+        NSLog("(publisherInterrupt) the msg: %@", msg)
+        publisherIsDisconnected = true
+        ALToastView.toast(in: self.view, withText:"publisherInterrupt: \(msg)")
+        
+        // Begin reconnect sequence...
+        reconnect()
+    }
+    
+    func findStreams() {
+        
+        let domain = Testbed.getParameter(param: "host") as! String
+        let app = Testbed.getParameter(param: "context") as! String
+        let urlPath = "http://" + domain + ":5080" + "/" + app + "/streams.jsp"
+        let streamName = Testbed.getParameter(param: "stream1") as! String
+        
+        var request = URLRequest(url: URL(string: urlPath)!)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        NSLog("Requesting stream list...")
+        
+        session.dataTask(with: request) {data, response, err in
+            
+            if err == nil {
+                //                let resut = data as String
+                do {
+                    NSLog("Stream list received...")
+                    //   Convert our response to a usable NSString
+                    let list = try JSONSerialization.jsonObject(with: data!) as! Array<Dictionary<String, String>>;
+                    
+                    var exists: Bool = false;
+                    for dict:Dictionary<String, String> in list {
+                        if(dict["name"] == streamName){
+                            exists = true;
+                            break;
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        if (exists) {
+                            NSLog("Publisher exists, let's try connecting...")
+                            self.Subscribe(streamName)
+                        }
+                        else {
+                            NSLog("Publisher does not exist.")
+                            self.reconnect()
+                        }
+                    }
+                    
+                }
+                catch let error as NSError {
+                    NSLog(error.localizedFailureReason!)
+                }
+            }
+            else {
+                NSLog(err!.localizedDescription)
+            }
+            
+            }.resume()
+        
+    }
+    
+    func reconnect () {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            if self.finished {
+                return
+            }
+            self.findStreams()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.finished = true
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidLoad() {
+        self.finished = false
+        super.viewDidLoad()
+        findStreams()
+    }
     
 }
 
