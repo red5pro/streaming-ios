2070c58c79d979e6ba4d7c42a65c221ac4b1222b
bustardcelly <bustardcelly@gmail.com>
2019-05-15 15:52:38 +0200

    Replacing Slide for Buttons in Publish Zoom test
    The slider at times could be over taken for a swipe action which dismisses the test entirely.
    Moved to more user friendly UIButton to zoom in and out.

--- R5ProTestbed/Tests/PublishZoomable/PublishZoomableTest.swift
+++ R5ProTestbed/Tests/PublishZoomable/PublishZoomableTest.swift
@@ -13,6 +13,7 @@ import R5Streaming
 class PublishZoomableTest: BaseTest {
 
     var cam : AVCaptureDevice? = nil;
+    var slider : UISlider? = nil;
     
     override func viewDidAppear(_ animated: Bool) {
 
@@ -38,22 +39,50 @@ class PublishZoomableTest: BaseTest {
         
         self.publishStream!.publish(Testbed.getParameter(param: "stream1") as! String, type: R5RecordTypeLive)
         
-        var slider = UISlider(frame: CGRect(x:20, y:UIScreen.main.bounds.height - 40, width:280, height:20))
-        slider.minimumValue = 1
-        slider.maximumValue = 5
-        slider.isContinuous = true
-        slider.tintColor = UIColor.red
-        slider.value = 1
-        slider.addTarget(self, action:  #selector(PublishZoomableTest.sliderValueDidChange(sender:)), for: .valueChanged)
-        self.view.addSubview(slider)
+        let screenSize = UIScreen.main.bounds.size
+        
+        slider = UISlider(frame: CGRect(x:90, y:UIScreen.main.bounds.height - 30, width:screenSize.width - 180, height:20))
+        slider?.minimumValue = 1
+        slider?.maximumValue = 5
+        slider?.isContinuous = true
+        slider?.tintColor = UIColor.red
+        slider?.value = 1
+        slider?.isEnabled = false
+//        slider?.addTarget(self, action:  #selector(PublishZoomableTest.sliderValueDidChange(sender:)), for: .valueChanged)
+        self.view.addSubview(slider!)
+        
+        let downBtn = UIButton(frame: CGRect(x: 20, y: screenSize.height - 40, width: 50, height: 34))
+        downBtn.backgroundColor = UIColor.darkGray
+        downBtn.setTitle("-", for: UIControl.State.normal)
+        view.addSubview(downBtn)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(onZoomOut))
+        downBtn.addGestureRecognizer(tap)
+        
+        let upBtn = UIButton(frame: CGRect(x: screenSize.width - 70, y: screenSize.height - 40, width: 50, height: 34))
+        upBtn.backgroundColor = UIColor.darkGray
+        upBtn.setTitle("+", for: UIControl.State.normal)
+        view.addSubview(upBtn)
+        let tapIn = UITapGestureRecognizer(target: self, action: #selector(onZoomIn))
+        upBtn.addGestureRecognizer(tapIn)
         
     }
     
-    @objc func sliderValueDidChange(sender:UISlider!) {
+    @objc func onZoomOut () {
+        let v = (slider?.value)! - 0.5 < 1 ? 1 : (slider?.value)! - 0.5
+        updateSliderValue(value: v)
+    }
+    
+    @objc func onZoomIn () {
+        let v = (slider?.value)! + 0.5 > 5 ? 5 : (slider?.value)! + 0.5
+        updateSliderValue(value: v)
+    }
+    
+    func updateSliderValue(value: Float!) {
         do {
             try cam!.lockForConfiguration()
-            cam!.ramp(toVideoZoomFactor: CGFloat(sender.value), withRate: 1.0)
+            cam!.ramp(toVideoZoomFactor: CGFloat(value), withRate: 1.0)
             cam!.unlockForConfiguration()
+            slider?.setValue(value, animated: true)
         } catch {
             //
         }
