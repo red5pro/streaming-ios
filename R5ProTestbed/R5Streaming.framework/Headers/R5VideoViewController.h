8180c1df8c3db29f15fa30c721f8617d93c239f5
beetlejesss <jessica@infrared5.com>
2020-01-31 17:43:20 -0500

    updated SDK to 6.0.0

--- R5ProTestbed/R5Streaming.framework/Headers/R5VideoViewController.h
+++ R5ProTestbed/R5Streaming.framework/Headers/R5VideoViewController.h
@@ -8,12 +8,12 @@
 
 #import <GLKit/GLKit.h>
 #import "R5Stream.h"
+#import "R5VideoViewRenderer.h"
 
 /**
  *  @brief The VideoView for all R5Streaming.  When publishing, it will contain the camera view.  While subscribing it will render all incoming stream data.  Streams will be cropped to fit the aspect ratio of the view.
  */
-@interface R5VideoViewController : UIViewController<GLKViewDelegate>
-	
+@interface R5VideoViewController : UIViewController<GLKViewDelegate, R5VideoViewRendererDelegate>
 
 /**
  *  Desired FPS to render the video at.  FPS lower than the streaming FPS will result in dropped frames.
@@ -26,6 +26,14 @@
  *  @param videoStream Stream to render
  */
 -(void) attachStream:(R5Stream *)videoStream;
+
+//
+///**
+// * Renderer hook invoked by R5VideoViewController to update the rendering display.
+// *
+// * @param view The underlying GLKView
+// */
+//- (void)updateRender:(GLKView *)view;
 
 /**
  *  Reset the GLES context and setup rendering loop
@@ -60,5 +68,12 @@
  * Scaling mode of the rendering view for subscribing streams
  */
 @property (nonatomic)  r5_scale_mode scaleMode;
+
+/**
+ * Set a custom renderer for the stream view
+ *
+ * @param renderer R5VideoViewRenderer
+ */
+@property (nonatomic, strong)   R5VideoViewRenderer *renderer;
 
 @end
